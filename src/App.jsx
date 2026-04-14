aaeee2d4739717047b820c2ce0b920db7abdf86a
RajkumarRj <rajpersonale@gmail.com>
2026-04-14 10:44:58 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState("");
+  const fetch = async () => {
+    const result = await axios.get("http://localhost:8080/");
+    console.log(result.data);
+    setData(result.data);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <>
+      <h1>{data}</h1>
+    </>
+  );
+}
+
+export default App;
